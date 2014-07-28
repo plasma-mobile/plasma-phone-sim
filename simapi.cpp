13892ce47f5f7b06a21cf61c9351b5ca469d9f21
Aaron Seigo <aseigo@kde.org>
2014-07-28 11:54:39 +0200

    create an object to hold simulation api to be used from QML

---
+++ simapi.cpp
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2014 Aaron Seigo <aseigo@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "simapi.h"
+
+SimApi::SimApi(const QString &currentPath, QObject *parent)
+    : QObject(parent),
+      m_packagePath(currentPath)
+{
+}
+
+QString SimApi::packagePath() const
+{
+    return m_packagePath;
+}
+
+void SimApi::setPackagePath(const QString &path)
+{
+    if (path != m_packagePath) {
+        m_packagePath = path;
+        emit packagePathChanged();
+    }
+}
+
+
+#include "simapi.moc"
---
+++ simapi.h
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2014 Aaron Seigo <aseigo@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <QObject>
+#pragma once
+
+class SimApi : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString packagePath READ packagePath WRITE setPackagePath NOTIFY packagePathChanged)
+
+public:
+    SimApi(const QString &currentPath, QObject *parent);
+
+    QString packagePath() const;
+    void setPackagePath(const QString &path);
+
+Q_SIGNALS:
+    void packagePathChanged();
+
+private:
+    QString m_packagePath;
+};
